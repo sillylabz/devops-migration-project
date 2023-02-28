158043d5f3d2fec1a9a87a8652cbcd1d6af3aa3f
Hernandez Aku <43397610+hernanku@users.noreply.github.com>
2023-02-27 20:11:58 -0500

    Develop (#10)
    * terragrunt source to github
    
    * update gitignore
    
    * updating git source
    
    * artifactory server
    
    * terraform artifactory backend
    
    * sensu backend config
    
    * jenkins docker build agent
    
    * aws ec2 instance
    
    * terragrunt aws ec2 autoscaling
    
    * sample ci-cd infra aws
    
    * jenkins playbooks
    
    * nexus repo
    
    * sonarqube aws infra
    
    * updating vcenter values
    
    * new vsphere details
    
    * gitlab server update terragrunt values
    
    * gitlab docker runner
    
    * local backend
    
    * sensu server terraform
    
    * eksa control server, local k8s infra

---
+++ infra/vsphere/eksa-control/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "local" {}
+}
+
+
+module "vm" {
+  source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
+  vsphere_server_url = "10.0.0.31"
+  vsphere_user       = "administrator@vsphere.local"
+  vsphere_password   = ""
+  vsphere_datacenter = "dc-east"
+  vsphere_cluster    = "dev-cluster"
+  vm_datastore = "vmData"
+  vm_name            = "eksa-control"
+  vm_disk_size = 50
+  vm_domain = "labz.io"
+  vm_ipv4_address = "10.0.0.32"
+  vm_gateway = "10.0.0.1"
+  vm_dns_server = "10.0.0.33"
+  // vm_count = 1
+  vm_network     = "vmPub"
+  vm_template    = "base-ubuntu22-templ"
+  vm_cpu         = 2
+  vm_memory      = 8192
+}
+
