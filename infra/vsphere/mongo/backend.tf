d5303c7e180491cce65d7f9e8619eb1323846eb4
Hernandez Anyiabey <akuhernandez@outlook.com>
2023-03-09 17:27:15 -0500

    refactor for security

--- infra/vsphere/mongo/backend.tf
+++ infra/vsphere/mongo/backend.tf
@@ -1,3 +1,3 @@
 terraform {
-  backend "artifactory" {}
+  backend "local" {}
 }
---
+++ infra/vsphere/mongo/main.tf
@@ -0,0 +1,21 @@
+
+module "mongo-server" {
+  source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
+  vsphere_server_url = "10.0.0.31"
+  vsphere_user       = "administrator@vsphere.local"
+  vsphere_password   = var.vsphere_password
+  vsphere_datacenter = "dc-east"
+  vsphere_cluster    = "dev-cluster"
+  vm_datastore = "vmData"
+  vm_name            = "mongo-server"
+  vm_disk_size = 50
+  vm_domain = "labz.io"
+  vm_ipv4_address = "10.0.0.36"
+  vm_gateway = "10.0.0.1"
+  vm_dns_server = "10.0.0.33"
+  // vm_count = 1
+  vm_network     = "vmPub"
+  vm_template    = "base-ubuntu22-templ"
+  vm_cpu         = 4
+  vm_memory      = 8192
+}
