81da587262a629eb98398eb655e2672907477e2e
Hernandez Aku <43397610+hernanku@users.noreply.github.com>
2023-03-30 14:42:59 -0400

    Develop (#14)
    * terragrunt source to github
    
    * update gitignore
    
    * updating git source
    
    * artifactory server
    
    * terraform artifactory backend
    
    * sensu backend config
    
    * jenkins docker build agent
    
    * aws ec2 instance
    
    * terragrunt aws ec2 autoscaling
    
    * sample ci-cd infra aws
    
    * jenkins playbooks
    
    * nexus repo
    
    * sonarqube aws infra
    
    * updating vcenter values
    
    * new vsphere details
    
    * gitlab server update terragrunt values
    
    * gitlab docker runner
    
    * local backend
    
    * sensu server terraform
    
    * eksa control server, local k8s infra
    
    * grafana server terraform
    
    * refactor for security
    
    * artifactory ansible
    
    * elasticsearch cluster infra
    
    * gitlab runner vsphere deployment
    
    * sample app server, openvpn-connector server

---
+++ infra/vsphere/app-server/main.tf
@@ -0,0 +1,22 @@
+
+module "gitlab-runner-docker" {
+  source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
+  vsphere_server_url = "192.168.1.11"
+  vsphere_user       = "administrator@vsphere.local"
+  vsphere_password   = var.vsphere_password
+  vsphere_datacenter = "dc-east"
+  vsphere_cluster    = "lab-cluster"
+  vm_datastore = "vmData"
+  vm_name            = "app-server1"
+  vm_disk_size = 50
+  vm_domain = "labz.io"
+  vm_ipv4_address = "192.168.1.63"
+  vm_gateway = "192.168.1.1"
+  vm_dns_server = "192.168.1.12"
+  vm_dns_search_domain = "labz.io"
+  // vm_count = 1
+  vm_network     = "vmPub"
+  vm_template    = "base-ubuntu22-templ-2"
+  vm_cpu         = 2
+  vm_memory      = 4092
+}
---
+++ infra/vsphere/openvpn-connector/main.tf
@@ -0,0 +1,22 @@
+
+module "openvpn-connector" {
+  source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
+  vsphere_server_url = "192.168.1.11"
+  vsphere_user       = "administrator@vsphere.local"
+  vsphere_password   = var.vsphere_password
+  vsphere_datacenter = "dc-east"
+  vsphere_cluster    = "lab-cluster"
+  vm_datastore = "vmData"
+  vm_name            = "openvpn-connector"
+  vm_disk_size = 50
+  vm_domain = "labz.io"
+  vm_ipv4_address = "192.168.1.13"
+  vm_gateway = "192.168.1.1"
+  vm_dns_server = "192.168.1.12"
+  vm_dns_search_domain = "labz.io"
+  // vm_count = 1
+  vm_network     = "vmPub"
+  vm_template    = "ubuntu22-templ-base-gold"
+  vm_cpu         = 2
+  vm_memory      = 4092
+}
