1cea4c3a15cd89a194a290814fa9c641e42ffe91
Hernandez Anyiabey <akuhernandez@outlook.com>
2023-03-31 09:58:17 -0400

    artifactory server, cicd runners server

--- infra/vsphere/runners-server/main.tf
+++ infra/vsphere/runners-server/main.tf
@@ -1,21 +1,22 @@
 
 module "gitlab-runner-docker" {
   source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
-  vsphere_server_url = "10.0.0.31"
+  vsphere_server_url = "192.168.1.11"
   vsphere_user       = "administrator@vsphere.local"
   vsphere_password   = var.vsphere_password
   vsphere_datacenter = "dc-east"
-  vsphere_cluster    = "dev-cluster"
+  vsphere_cluster    = "lab-cluster"
   vm_datastore = "vmData"
-  vm_name            = "gitlab-runner"
+  vm_name            = "runners-server"
   vm_disk_size = 50
   vm_domain = "labz.io"
-  vm_ipv4_address = "10.0.0.79"
-  vm_gateway = "10.0.0.1"
-  vm_dns_server = "10.0.0.33"
+  vm_ipv4_address = "192.168.1.20"
+  vm_gateway = "192.168.1.1"
+  vm_dns_server = "192.168.1.12"
+  vm_dns_search_domain = "labz.io"
   // vm_count = 1
   vm_network     = "vmPub"
-  vm_template    = "base-ubuntu22-templ"
+  vm_template    = "base-ubuntu22-templ-2"
   vm_cpu         = 2
   vm_memory      = 4092
 }
