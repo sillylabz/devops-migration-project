8d55c9dcff9ec5b50ec73d9c16b96201c1a92794
Hernandez Anyiabey <akuhernandez@outlook.com>
2023-04-11 14:22:49 -0400

    jenkins ansible playbook

--- infra/vsphere/jenkins/main.tf
+++ infra/vsphere/jenkins/main.tf
@@ -1,5 +1,5 @@
 
-module "app-server1" {
+module "jenkins" {
   source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
   vsphere_server_url = "192.168.1.11"
   vsphere_user       = "administrator@vsphere.local"
@@ -7,16 +7,16 @@ module "app-server1" {
   vsphere_datacenter = "dc-east"
   vsphere_cluster    = "lab-cluster"
   vm_datastore = "vmData"
-  vm_name            = "app-server1"
+  vm_name            = "jenkins"
   vm_disk_size = 50
   vm_domain = "labz.io"
-  vm_ipv4_address = "192.168.1.63"
+  vm_ipv4_address = "192.168.1.43"
   vm_gateway = "192.168.1.1"
   vm_dns_server = "192.168.1.12"
   vm_dns_search_domain = "labz.io"
   // vm_count = 1
   vm_network     = "vmPub"
-  vm_template    = "ubuntu22-templ-base-gold"
+  vm_template    = "ubuntu22-base-templ-gold"
   vm_cpu         = 2
   vm_memory      = 4092
 }
