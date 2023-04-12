94aaecbb6deab10270bf49f4eb312380c3fa9c4c
hernanku <akuhernandez@outlook.com>
2023-04-12 19:46:29 -0400

    app server fix

--- infra/vsphere/app-server/main.tf
+++ infra/vsphere/app-server/main.tf
@@ -10,16 +10,16 @@ module "app-server1" {
   vm_name            = "app-server1"
   vm_disk_size = 50
   vm_domain = "labz.io"
-  vm_ipv4_address = "192.168.1.43"
+  vm_ip = "192.168.1.46"
   vm_gateway = "192.168.1.1"
   // vm_count = 1
   vm_network     = "vmPub"
-  vm_template    = "rocky9-base-templ-gold-1"
+  vm_template    = "centos7-base-templ-gold"
   vm_cpu         = 1
   vm_memory      = 2048
   vm_customize_timeout = 2
-  vm_baseip      = "192.168.1"
-  vm_ip_suffix   = "40"
+  // vm_baseip      = "192.168.1"
+  // vm_ip_suffix   = "40"
   vm_netmask     = 24
   vm_dns_servers = ["192.168.1.12"]
 
