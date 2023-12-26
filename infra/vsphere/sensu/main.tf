699e66ea5f0a7a39724a060c30b789ab00227ec5
Hernandez Anyiabey <akuhernandez@outlook.com>
2023-12-26 15:31:21 -0500

    :sparkles: sensu server

--- infra/vsphere/sensu/main.tf
+++ infra/vsphere/sensu/main.tf
@@ -1,26 +1,21 @@
-terraform {
-  backend "local" {}
-}
-
-
-module "vm" {
-  source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
-  vsphere_server_url = "10.0.0.31"
-  vsphere_user       = "administrator@vsphere.local"
-  vsphere_password   = ""
-  vsphere_datacenter = "dc-east"
-  vsphere_cluster    = "dev-cluster"
-  vm_datastore = "vmData"
-  vm_name            = "sensu"
-  vm_disk_size = 50
-  vm_domain = "labz.io"
-  vm_ipv4_address = "10.0.0.77"
-  vm_gateway = "10.0.0.1"
-  vm_dns_server = "10.0.0.33"
-  // vm_count = 1
-  vm_network     = "vmPub"
-  vm_template    = "base-ubuntu22-templ"
-  vm_cpu         = 2
-  vm_memory      = 4096
+module "sensu" {
+  source               = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
+  vsphere_server_url   = "10.23.1.11"
+  vsphere_user         = "administrator@vsphere.local"
+  vsphere_password     = var.vsphere_password
+  vsphere_datacenter   = "dc-east"
+  vsphere_cluster      = "lab-cluster"
+  vm_datastore         = "vmData"
+  vm_name              = "sensu"
+  vm_disk_size         = 50
+  vm_dns_search_domain = "labz.io"
+  vm_domain            = "labz.io"
+  vm_ipv4_address      = "10.23.1.16"
+  vm_gateway           = "10.23.1.1"
+  vm_network           = "vmPub"
+  vm_template          = "gold-templ-ubuntu22-cloudinit"
+  vm_cpu               = 1
+  vm_memory            = 2048
+  vm_dns_server        = "10.23.1.9"
 }
 
