bf2040bf5e50e3141512f1a3e9e3cd96ad235dc4
Hernandez Anyiabey <akuhernandez@outlook.com>
2023-12-25 23:28:46 -0500

    :sparkles: working vm cloudinit ubuntu template

--- infra/vsphere/app-server/main.tf
+++ infra/vsphere/app-server/main.tf
@@ -1,7 +1,7 @@
 
 module "app-server1" {
-  source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm"
-  vsphere_server_url = "192.168.1.11"
+  source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
+  vsphere_server_url = "10.23.1.11"
   vsphere_user       = "administrator@vsphere.local"
   vsphere_password   = var.vsphere_password
   vsphere_datacenter = "dc-east"
@@ -9,18 +9,14 @@ module "app-server1" {
   vm_datastore = "vmData"
   vm_name            = "app-server1"
   vm_disk_size = 50
+  vm_dns_search_domain = "labz.io"
   vm_domain = "labz.io"
-  vm_ip = "192.168.1.46"
-  vm_gateway = "192.168.1.1"
-  // vm_count = 1
+  vm_ipv4_address = "10.23.1.46"
+  vm_gateway = "10.23.1.1"
   vm_network     = "vmPub"
-  vm_template    = "centos7-base-templ-gold"
+  vm_template    = "gold-templ-ubuntu22-cloudinit"
   vm_cpu         = 1
   vm_memory      = 2048
-  vm_customize_timeout = 2
-  // vm_baseip      = "192.168.1"
-  // vm_ip_suffix   = "40"
-  vm_netmask     = 24
-  vm_dns_servers = ["192.168.1.12"]
+  vm_dns_server = "10.23.1.9"
 
 }
