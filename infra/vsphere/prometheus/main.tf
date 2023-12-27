570d557ee9e650040bfce6c89b345e3989a22fd2
Hernandez Anyiabey <akuhernandez@outlook.com>
2023-12-27 06:27:01 -0500

    :bug: prom server ip

--- infra/vsphere/prometheus/main.tf
+++ infra/vsphere/prometheus/main.tf
@@ -1,4 +1,4 @@
-module "sensu" {
+module "prometheus" {
   source               = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
   vsphere_server_url   = "10.23.1.11"
   vsphere_user         = "administrator@vsphere.local"
@@ -10,7 +10,7 @@ module "sensu" {
   vm_disk_size         = 50
   vm_dns_search_domain = "labz.io"
   vm_domain            = "labz.io"
-  vm_ipv4_address      = "10.23.1.17"
+  vm_ipv4_address      = "10.23.1.18"
   vm_gateway           = "10.23.1.1"
   vm_network           = "vmPub"
   vm_template          = "gold-templ-ubuntu22-cloudinit"
