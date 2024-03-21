a2da71e171e085224d52b99740f2b53fc2ef4a33
hernanku <akuhernandez@outlook.com>
2024-03-21 05:52:22 -0400

    :sparkles: elasticsearch and kibana servers

--- infra/vsphere/kibana/main.tf
+++ infra/vsphere/kibana/main.tf
@@ -1,5 +1,5 @@
 
-module "elasticsearch-master" {
+module "kibana-server" {
   source = "git::https://github.com/hernanku/infra-devops.git//terraform/vmware/vsphere/single-vm-cloud"
   vsphere_server_url = "10.23.1.11"
   vsphere_user       = "administrator@vsphere.local"
@@ -7,16 +7,16 @@ module "elasticsearch-master" {
   vsphere_datacenter = "dc-east"
   vsphere_cluster    = "lab-cluster"
   vm_datastore = "vmData"
-  vm_name            = "elasticsearch-master"
+  vm_name            = "kibana-server"
   vm_disk_size = 200
   vm_dns_search_domain = "labz.io"
   vm_domain = "labz.io"
-  vm_ipv4_address = "10.23.1.20"
+  vm_ipv4_address = "10.23.1.17"
   vm_gateway = "10.23.1.1"
   vm_network     = "vmPub"
   vm_template    = "gold-templ-ubuntu22-cloudinit"
   vm_cpu         = 2
-  vm_memory      = 8192
+  vm_memory      = 4096
   vm_dns_server = "10.23.1.9"
 
 }
